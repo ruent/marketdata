5497745beaa13cb3b33bf99117b6321599547243
ruent <ed784@nyu.edu>
2020-07-30 23:04:02 -0400

    m

--- mdata.h
+++ mdata.h
@@ -7,10 +7,12 @@ using namespace std;
 //price date
 //calling functions can calculate the time multiple and retrive the
 //stored numbers
-
+//endTImes = [0,1,2, +1m, +2m, ...]
+//year fractions should be put here assuming a daycount
+//no daycount based calculations should/can appear here
 struct timezero
 {
     const vector<double>& tau; //period year fractions
-    const vector<double>& endTimes;  
+    const vector<unsigned int>& endTimes;  
     const vector<double>& discVector; //a discount value for each endtime
 };
