b5fdcdbe16d8d64211f12448eb2ca78205e1c9be
ruent <ed784@nyu.edu>
2020-10-24 23:13:00 -0400

    some commit

--- mdata.h
+++ mdata.h
@@ -2,44 +2,14 @@
 #include <vector>
  
 
-//since I cannot get this data natively
-//I'll cache it here in this struct
-//I'll assume data for each +1month, relative to
-//price date, except the INITIAL POINT
+//since I cannot get this data natively, I'll cache it here 
 
-
-//SO ALL DISC,TAU ARE EQUAL LENGTH VECTORS ...
-//calling functions can calculate the time multiple and retrieve the
-//stored numbers
- 
-//A swap's payment dates will simple be the same, 
-//e.g. 5x5 swap will have payment dates as
-// [63,66, ..., 120]
-// such a swap come and look for the discount factor here
-
+//depending on payment dates etc. data should be made available here 
 //year fractions should be put here assuming a daycount
 //no daycount based calculations should/can appear here
 //I might need to be several tau vectors here where daycount
 // or freq change  
-//default will be something like [0.5, 0.5 , ...]
-//an entry for each month, giving year fraction with 
-//the PREVIOUS 6 months
 
-//if a swaption has paym date 12 (i.e. at 1year), then disc[12]
-//is right if disc has a zero entry, meaning it should start from 0th month
-//this should also be the case for tau
-
-
-//tau[i] is the tau
-//for the interval endtimes[i] and endtimes[i-1] 
-
-//make vol data also in this format
-//as long as lin-interp is used, interpolating 1monhly data
-//should be ok.
-//alternative is to have a dictinonary structure:
-// vol at 3m, at 1year etc.
-
-//vol can be normal vol
 struct timezero
 {
      std::map<size_t, std::vector<double>, std::less<size_t>> data;
@@ -64,6 +34,6 @@ struct timezero
  
 struct somedata
 {
-    std::unique_ptr<double> xyz;
+    //std::unique_ptr<double> xyz;
 };
  
